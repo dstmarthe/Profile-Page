8e0b8b1c29de515d237e3e5fcf6890fb51f56b11
dSTM <dstmarthe@users.noreply.github.com>
2020-11-30 23:48:33 -0500

    Starting JS code to fetch github reps

--- projectsInfo.js
+++ projectsInfo.js
@@ -1,25 +1,53 @@
 'use strict';
 import styled from 'styled-components';
 
-const boverText = styled.div`
-	color: #000;
-	:hover {
-		color: #ed1212;
-		cursor: pointer;
-	}
+const api= 
+
+const hoverWin = styled.div`
+display: inline-block;
+border-radius: 3px;
+padding: 0.5rem 0;
+margin: auto;
+width: 16rem;
+background-color: white;
+color: black;
 `
-const e = React.createElement;
+function requestUserRepos(){
+    
+  // create new XMLHttpRequest object
+  const xhr = new XMLHttpRequest();
+  
+  // GitHub endpoint, dynamically passing in specified username
+  const url = `https://api.github.com/users/${username}/repos`;
+  
+  // Open a new connection, using a GET request via URL endpoint
+  // Providing 3 arguments (GET/POST, The URL, Async True/False)
+  xhr.open('GET', url, true);
+  
+  xhr.onload = function() {
+    
+    // Parse API data into JSON
+    const data = JSON.parse(this.response);
+    
+    // Log the response
+    console.log(data);
 
-class LikeButton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { liked: false };
-  }
+}
 
-  render() {
-    if (this.state.liked) {
-      return 'You liked this.';
-    }
+// Send the request to the server
+xhr.send();
+
+}
+
+// Call function passing in 'dstmarthe' as GitHub username
+requestUserRepos('dstmarthe');
+
+render(
+  <div>
+  <h2>${name}</h2>
+  <p></p>
+</div>
+)
 
     return e(
       'button',

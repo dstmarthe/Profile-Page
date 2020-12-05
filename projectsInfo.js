efd16927eeb1df0cd75545131bb333dbbe376bee
dSTM <dstmarthe@users.noreply.github.com>
2020-12-04 21:52:54 -0500

    React render fixes

--- projectsInfo.js
+++ projectsInfo.js
@@ -1,29 +1,26 @@
 "use strict";
-import styled from "styled-components";
-import React from "practical-react";
-import ReactDOM from "practical-react"
-var React = require('react');
-var ReactDOM = require('react-dom');
+var React = require("react");
+var ReactDOM = require("react-dom");
 const domContainer = document.getElementById("projInfo");
 
-function hover(element, enter, leave) {
-	element.addEventListener("mouseenter", enter);
-	element.addEventListener("mouseleave", leave);
-}
+// function hover(element, enter, leave) {
+// 	element.addEventListener("mouseenter", enter);
+// 	element.addEventListener("mouseleave", leave);
+// }
 
-hover(
-	document.getElementsByClassName("project-link"),
-	(e) => {
-		// On hover
-		ReactDOM.render(<projectsInfo />, domContainer);
-	},
-	(e) => {
-		// On exit hover
-		return null;
-	}
-);
+// hover(
+// 	document.querySelectorAll("project-link"),
+// 	(e) => {
+// 		// On hover
+// 		ReactDOM.render(<projectsInfo />, domContainer);
+// 	},
+// 	(e) => {
+// 		// On exit hover
+// 		return null;
+// 	}
+// );
 
-export default class projectsInfo extends React.component {
+export default class projectsInfo extends React.Component {
 	state = {
 		loading: true,
 		person: null,
@@ -37,16 +34,18 @@ export default class projectsInfo extends React.component {
 		this.setState({ person: data.results[0], loading: false });
 	}
 
-	render(){ 
-		<div>
-			(this.state.loading || !this.state.person) ?  
-				<div>loading...</div></div>
-			 ): (
-				<div>
-					<h2>{this.state.person.name}</h2>
-					<p>{this.state.person.description}</p>
-				</div>
-			);
-		}
+	render() {
+		return (
+			<>
+				{this.state.loading || !this.state.person ? (
+					<p>loading...</p>
+				) : (
+					<>
+						<h2>{this.state.person.name}</h2>
+						<p>{this.state.person.description}</p>
+					</>
+				)}
+			</>
+		);
 	}
 }

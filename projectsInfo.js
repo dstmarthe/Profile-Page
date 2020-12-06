a8fd99a5421eec2c9b22530e29fbaf0306463bf9
dSTM <dstmarthe@users.noreply.github.com>
2020-12-05 21:08:20 -0500

    Uninstall Raphael.

--- projectsInfo.js
+++ projectsInfo.js
@@ -1,6 +1,4 @@
 "use strict";
-import React from "react";
-import ReactDOM from "react-dom";
 var React = require("react");
 var ReactDOM = require("react-dom");
 const domContainer = document.getElementById("#projInfo");
@@ -10,11 +8,49 @@ function hover(element, enter, leave) {
 	element.addEventListener("mouseleave", leave);
 }
 
+class projectsInfo extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			items: [],
+			isLoaded: false,
+		};
+	}
+
+	componentDidMount() {
+		fetch("https://api.github.com/users/dstmarthe/repos")
+			.then((res) => res.json())
+			.then((json) => {
+				this.setState({
+					isLoaded: true,
+					items: json,
+				});
+			});
+	}
+
+	render() {
+		var { isLoaded, items } = this.state;
+		if (!isLoaded) {
+			return <div>Loading...</div>;
+		} else {
+			return (
+				<div className="info">
+					<ul>
+						{items.mao((item) => (
+							<li key={item.id}>Project Name: {item.name}</li>
+						))}
+					</ul>
+				</div>
+			);
+		}
+	}
+}
+
 hover(
-	document.querySelectorAll('.project-link'),
+	document.querySelectorAll(".project-link"),
 	(e) => {
 		// On hover
-		ReactDOM.render(<projectsInfo />, domContainer);
+		ReactDOM.render(info, domContainer);
 		console.log("Hovering");
 	},
 	(e) => {
@@ -22,33 +58,3 @@ hover(
 		return null;
 	}
 );
-
-export default class projectsInfo extends React.Component {
-	state = {
-		loading: true,
-		person: null,
-	};
-
-	async componentDidMount() {
-		const url = "https://api.github.com/users/dstmarthe/repos";
-		const response = await fetch(url);
-		const data = response.json;
-		console.log(data.results[0]);
-		this.setState({ person: data.results[0], loading: false });
-	}
-
-	render() {
-		return (
-			<>
-				{this.state.loading || !this.state.person ? (
-					<p>loading...</p>
-				) : (
-					<>
-						<h2>{this.state.person.name}</h2>
-						<p>{this.state.person.description}</p>
-					</>
-				)}
-			</>
-		);
-	}
-}

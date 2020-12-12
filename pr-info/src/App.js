aac8863571d6c9943ddc5ea1368e77727727f8c2
dSTM <dstmarthe@users.noreply.github.com>
2020-12-11 20:12:57 -0500

    +Babel files

--- pr-info/src/App.js
+++ pr-info/src/App.js
@@ -1,42 +1,41 @@
-import './style.css';
-import React from 'react';
-  
-  class App extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        items: [],
-        isLoaded: false,
-      };
-    }
-  
-    componentDidMount() {
-      fetch("https://api.github.com/users/dstmarthe/repos")
-        .then((res) => res.json())
-        .then((json) => {
-          this.setState({
-            isLoaded: true,
-            items: json,
-          });
-        });
-    }
-  
-    render() {
-      var { isLoaded, items } = this.state;
-      if (!isLoaded) {
-        return <div>Loading...</div>;
-      } else {
-        return (
-          <div className="info">
-            <ul>
-              {items.map((item) => (
-                <li key={item.id}>Project Name: {item.name}</li>
-              ))}
-            </ul>
-          </div>
-        );
-      }
-    }
-  }
+import "./style.css";
+import React from "react";
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			items: [],
+			isLoaded: false,
+		};
+	}
+
+	componentDidMount() {
+		fetch("https://api.github.com/users/dstmarthe/repos")
+			.then((res) => res.json())
+			.then((json) => {
+				this.setState({
+					isLoaded: true,
+					items: json,
+				});
+			});
+	}
+
+	render() {
+		var { isLoaded, items } = this.state;
+		if (!isLoaded) {
+			return <div>Loading...</div>;
+		} else {
+			return (
+				<div className="info">
+					<ul>
+						{items.map((item) => (
+							<li key={item.id}>Project Name: {item.name}</li>
+						))}
+					</ul>
+				</div>
+			);
+		}
+	}
+}
 
 export default App;

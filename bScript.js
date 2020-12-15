047bc59a4478719cba0180f5e839d7c04b587e98
dSTM <dstmarthe@users.noreply.github.com>
2020-12-15 00:57:58 -0500

    bScript can now fetch api info to console

--- bScript.js
+++ bScript.js
@@ -1,29 +1,28 @@
-var domContainer = document.getElementById("#projInfo");
+const domContainer = document.querySelector("#projInfo");
+const searchQueryURL = "https://api.github.com/users/dstmarthe/repos";
+domContainer.innerHTML = "";
 
 function hover(element, enter, leave) {
 	element.forEach((link) => link.addEventListener("mouseenter", enter));
 	element.forEach((link) => link.addEventListener("mouseleave", leave));
 }
 
-function getRepo() {
-	fetch("https://api.github.com/users/dstmarthe/repos")
-		.then((res) => res.json())
-		.then((data) => {
-			console.log(data.name);
-			domContainer.insertAdjacentHTML(
-				//Needs new insertion method
-				"afterbegin",
-				`<p>Repo Name: ${data.name} </p>`
-			);
-		});
+async function getRepo(num) {      //get repo info by index
+	return fetch(searchQueryURL)
+		.then((result) => result.json())
+		.then((response) => {
+			console.log(response[num].name)
+			return response[num].name;  //returm name of repo by index
+		})
+		.catch((err) => console.log(err));
 }
 
-hover(
+hover(  //call Hover function
 	document.querySelectorAll(".project-tile"),
 	(e) => {
 		// On hover
 		console.log("Hovering");
-		getRepo();
+		domContainer.innerhtml = getRepo(3);
 	},
 	(e) => {
 		// On exit hover

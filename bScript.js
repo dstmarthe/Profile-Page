f2d649eff384c1871a67e8c912478d3b6b793a13
dSTM <dstmarthe@users.noreply.github.com>
2020-12-15 21:18:07 -0500

    Repo info now displays on page

--- bScript.js
+++ bScript.js
@@ -7,22 +7,27 @@ function hover(element, enter, leave) {
 	element.forEach((link) => link.addEventListener("mouseleave", leave));
 }
 
-async function getRepo(num) {      //get repo info by index
-	return fetch(searchQueryURL)
+async function getRepo(num) {
+	//get repo info by index
+	fetch(searchQueryURL)
 		.then((result) => result.json())
 		.then((response) => {
-			console.log(response[num].name)
-			return response[num].name;  //returm name of repo by index
+			console.log(response[num].name);
+			//returm name and description of repo by index
+			domContainer.innerHTML = `Name: ${response[num].name}
+			\<br/ > Description: ${response[num].description}`
+
 		})
 		.catch((err) => console.log(err));
 }
 
-hover(  //call Hover function
+hover(
+	//call Hover function
 	document.querySelectorAll(".project-tile"),
 	(e) => {
 		// On hover
 		console.log("Hovering");
-		domContainer.innerhtml = getRepo(3);
+		getRepo(1);
 	},
 	(e) => {
 		// On exit hover

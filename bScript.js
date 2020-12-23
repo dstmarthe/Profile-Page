7642c44f898128944804ba1b1e32e7592761044b
dSTM <dstmarthe@users.noreply.github.com>
2020-12-22 22:18:32 -0500

    Added links

--- bScript.js
+++ bScript.js
@@ -5,6 +5,26 @@ domContainer.innerHTML = "";
 CSS.paintWorklet.addModule("houdini-static-gradient/worklet.js");
 CSS.paintWorklet.addModule("bytemare/bytemare.js");
 
+const colorPallete = [
+	"#fa744f",
+	"#ffbcbc",
+	"#4cd3c2",
+	"#b7efcd",
+	"#edffea",
+	"#ffe75e",
+	"#feb72b",
+	"#e85f99",
+	"#9399ff",
+	"#dab8f3",
+	"#ea7ad7",
+	"#ce0f3d",
+	"#fd2eb3",
+];
+window.onload = function () {
+	document.querySelector("main").style.setProperty("--bytemare-color", color1);
+	const color1 = colorPalette[Math.floor(Math.random() * colorPalette.length)];
+};
+
 function hover(element, enter, leave) {
 	element.forEach((link) => link.addEventListener("mouseenter", enter));
 	element.forEach((link) => link.addEventListener("mouseleave", leave));
@@ -17,15 +37,19 @@ async function getRepo(num) {
 		.then((response) => {
 			console.log(response[num].name);
 			//returm name and description of repo by index
-			domContainer.innerHTML = `Name: ${response[num].name}
-			\<br/ > Description: ${response[num].description}`;
+			domContainer.innerHTML = `<em>Name:</em> ${response[num].name.replace("-", " ")}
+			\<br/ > <br/ > <em>Description:</em> ${response[num].description}`;
 		})
 		.catch((err) => console.log(err));
 }
 
+var node;
+function changeNode(num) {
+	node = +num;
+}
 hover(
 	//call Hover function
-	...linkList,
+	linkList,
 	(e) => {
 		// On hover
 		console.log("Hovering");
